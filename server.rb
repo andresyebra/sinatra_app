96bb91225740805ea607ec988da7fd3f4880eb6f
Andres Yebra <andres.yebra@bigcommerce.com>
2021-08-27 11:13:17 -0700

    Removing blank spaces in server.rb file
    Added comment to get routes in server.rb file
    
    Added comment to post routes in server.rb file

--- server.rb
+++ server.rb
@@ -3,27 +3,25 @@ require 'bundler/setup'
 require 'sinatra'
 
 #Settings
-
 #Port: 3000
 set :port, 3000 
 
 #Environment: production
-set :environment, :production 
-
-
+set :environment, :production
 #Routes 
 
 #GET
+#Get method
 get '/' do
   "Hello world, it's #{Time.now} at the server!"
 end
-
+#Get method with params
 get '/params' do
   "GET Params:#{params}"
 end
 
-
 #POST
+# POST method with params
 post '/params' do
   "POST Params:#{params}"
 end
